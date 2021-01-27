d8e5b58704ceda29709c484563ad385e0b047675
all-an <all.ancapistan@gmail.com>
2021-01-27 18:19:12 -0300

    learnging

--- english.py
+++ english.py
@@ -1,20 +1,23 @@
 word1 = "Butterscotch"
-sentence1 = "The man runs into the house and hurries through the cabinets and shelves. He finds and takes out an hourglass."
+sentence1 = "He's rummaging through the cabinets and shelves inside the house."
+
+def part2():
+    attempt2 = input("Ele está vasculhando os armários e prateleiras dentro da casa.")
+    if attempt2 == sentence1:
+        print("Congratulations!")
+    else:
+        print("Wrong, correct is >>>", sentence1)
+        print("Try again !")
+        part2() 
 
 def game():
     attempt = input("Manteiga de Caramelo: ")
     if attempt == word1:
         print("Congratulations!")
+        part2()
     else:
         print("Wrong, correct is Butterscotch")
         print("Try again !")
-        game()
-    attempt2 = input("O homem entra correndo na casa e vasculha com pressa os armários e estantes. Ele encontra e pega uma ampulheta.: ")
-    if attempt2 == sentence1:
-        print("Congratulations!")
-    else:
-        print("Wrong, correct is Butterscotch")
-        print("Try again !")
-        game()    
+        game()   
 
 game()
